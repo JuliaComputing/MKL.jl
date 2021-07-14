89c5ef3eebe3618781afbbe0af143c1bcd8fff5f
Carsten Bauer <crstnbr@gmail.com>
2021-07-14 19:59:36 +0200

    add mkl_provider option

--- src/MKL.jl
+++ src/MKL.jl
@@ -1,6 +1,48 @@
 module MKL
 
-using MKL_jll
+using Preferences
+using Libdl
+
+# Choose an MKL provider; taking an explicit preference as the first choice,
+# but if nothing is set as a preference, fall back to an environment variable,
+# and if that is not given, fall back to the default choice of `MKL_jll`.
+const mkl_provider = lowercase(something(
+    @load_preference("mkl_provider", nothing),
+    get(ENV, "JULIA_MKL_PROVIDER", nothing),
+    "mkl_jll",
+)::String)
+
+if mkl_provider == "mkl_jll"
+    # Only load MKL_jll if we are suppoed to use it as the MKL source
+    # to avoid an unnecessary download of the (lazy) artifact.
+    import MKL_jll
+    const libmkl_rt = MKL_jll.libmkl_rt
+    const mkl_path = dirname(libmkl_rt)
+elseif mkl_provider == "system"
+    # We want to use a "system" MKL, so let's try to find it.
+    # The user may provide the path to libmkl_rt via a preference
+    # or an environment variable. Otherwise, we expect it to
+    # already be loaded, or be on our linker search path.
+    const mkl_path = lowercase(something(
+        @load_preference("mkl_path", nothing),
+        get(ENV, "JULIA_MKL_PATH", nothing),
+        "",
+    )::String)
+    const libmkl_rt = find_library(["libmkl_rt"], [mkl_path])
+    libmkl_rt == "" && error("Couldn't find libmkl_rt. Maybe set JULIA_MKL_PATH?")
+else
+    error("Invalid mkl_provider choice $(mkl_provider).")
+end
+
+# Changing the MKL provider preference
+function set_mkl_provider(provider)
+    if lowercase(provider) ∉ ("mkl_jll", "system")
+        error("Invalid mkl_provider choice $(provider)")
+    end
+    @set_preferences!("mkl_provider" => lowercase(provider))
+
+    @info("New MKL provider set; please restart Julia to see this take effect", provider)
+end
 
 JULIA_VER_NEEDED = v"1.7.0-DEV.641"
 VERSION > JULIA_VER_NEEDED && using LinearAlgebra
@@ -38,11 +80,11 @@ function set_interface_layer(interface = Base.USE_BLAS64 ? INTERFACE_ILP64 : INT
 end
 
 function __init__()
-    if MKL_jll.is_available()
-        set_threading_layer()
-        set_interface_layer()
-        VERSION > JULIA_VER_NEEDED && BLAS.lbt_forward(libmkl_rt, clear=true)
-    end
+    # if MKL_jll.is_available()
+    set_threading_layer()
+    set_interface_layer()
+    VERSION > JULIA_VER_NEEDED && BLAS.lbt_forward(libmkl_rt, clear=true)
+    # end
 end
 
 function mklnorm(x::Vector{Float64})
@@ -50,7 +92,5 @@ function mklnorm(x::Vector{Float64})
           (Ref{MKLBlasInt}, Ptr{Float64}, Ref{MKLBlasInt}),
           length(x), x, 1)
 end
-
-VERSION > JULIA_VER_NEEDED && include("install.jl")
 
 end # module
